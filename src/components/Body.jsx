f990688420630bfe40e388ee99c4f88803e00171
Ryan Bass <theryanbass@gmail.com>
2022-09-03 23:04:01 -0700

    add additional styling, filler test text

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,18 @@
 function Body() {
-  return ( <p>BODY TEXT</p> );
+  return ( 
+    <>
+      <div className="body-style">
+        <p>HERE AT EMMETTECH iNDuStRies sometimes we like to aLterNate uppercase and lowercase lettering. Like they did in the early 00's.</p>
+        <p>Which is annoying as shit.</p>
+        <p>But our installs aren't. They're fucking quality.</p>
+        <p>Here's a random stock image Justin found on the internet to represent that quality on our OG site:</p>
+        <img src="https://maleeksperten.dk/wp-content/uploads/2018/04/koekken-belysning-350x260.jpg"></img>
+        <p>DoN'T you worry THO. Ryan's gonna MAKe thIs So dOpE</p>
+      </div>
+    </>
+
+
+   );
 }
 
 export default Body;
